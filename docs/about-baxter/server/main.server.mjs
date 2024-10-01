4d0664b6dfb3b21ca95da3faf93871bf32fa0f34
Baxter <baj55@georgetown.edu>
2024-09-30 22:16:13 -0400

    changed output to docs in angular.json

--- docs/about-baxter/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-CER4HQKJ.mjs";import"./chunk-5Q333IUT.mjs";import"./chunk-OQKR7A6Q.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
--- docs/about-baxter/server/render-utils.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{i as o,j as r,k as n}from"./chunk-5Q333IUT.mjs";import{jb as e}from"./chunk-OQKR7A6Q.mjs";import"./chunk-DCFYTP2A.mjs";export{n as renderApplication,r as renderModule,e as \u0275Console,o as \u0275SERVER_CONTEXT};
