4b8e7c6c809ea504552e226256b0e7181465c290
Baxter <baj55@georgetown.edu>
2024-10-01 00:54:42 -0400

    changed outputpath to fix

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-CER4HQKJ.mjs";import"./chunk-5Q333IUT.mjs";import"./chunk-OQKR7A6Q.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
+import{a}from"./chunk-7FDRD4AK.mjs";import"./chunk-5Q333IUT.mjs";import"./chunk-OQKR7A6Q.mjs";import"./chunk-DCFYTP2A.mjs";export{a as default};
